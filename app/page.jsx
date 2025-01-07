c7e767eedf5166edf6b7b08362dc8011215017a3
nohahakim <raafatabass@hotmail.com>
2025-01-07 20:38:35 +0300

    Routing

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,12 @@
-const HomePage = () => {
-  return <div className="text-2xl">Home Page</div>;
-};
+import Link from "next/link";
 
-export default HomePage;
+// Home page component
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className="text-3xl">Welcome</h1> {/* Page heading */}
+      {/* Link to the Properties page */}
+      <Link href="/properties">Go to Properties</Link>
+    </div>
+  );
+}
