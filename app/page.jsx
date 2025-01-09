9a38195c0b2cd9ef0c2bde7dc5581b9cd8fd6b01
nohahakim <raafatabass@hotmail.com>
2025-01-09 09:37:51 +0300

    homeProperties

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
 // Home page component
 export default function HomePage() {
@@ -8,6 +9,7 @@ export default function HomePage() {
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 }
