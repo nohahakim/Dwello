97a55baef8c52647efd24d0a30454dbe37458924
nohahakim <raafatabass@hotmail.com>
2025-01-12 09:59:11 +0300

    propertymap

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropertyMap from "./PropertyMap";
 import {
   FaTimes,
   FaBed,
@@ -88,7 +89,7 @@ const PropertyDetails = ({ property }) => {
         </ul>
       </div>
       <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-        <div id="map"></div>
+        <PropertyMap property={property} />
       </div>
     </main>
   );
